cc6b7e93586b9f60cdb3d9ff33e4e8825e18cfb3
caissecheztao <alexidjx@gmail.com>
2026-05-16 10:47:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE = "caisse-v1";
-const FILES = ["/caisse/", "/caisse/index.html", "/caisse/manifest.json", "/caisse/icon.png"];
+const CACHE = "caisse-v2";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
